b3f8e45f723ed785ec5da0319ca87b9922c66228
ggolan14 <guy.golan@ymail.com>
2024-01-23 11:00:57 +0200

    allow click only in up side of the land

--- client/src/components/content/queen_garden/game_board/land/land.js
+++ client/src/components/content/queen_garden/game_board/land/land.js
@@ -5,21 +5,35 @@ import {CastleName} from "../castle/castle_name";
 import {QueenGardenGameBoardContext} from "../../context/qg_board_context";
 
 export const Land = ({side}) => {
+  console.log("---> side="+side);
+  
   const {gameState, changeWagonPlace} = useContext(QueenGardenGameBoardContext);
 
   const allowClick = !gameState.disable_castle_click;
 
-  const onClick = () => {
-    if (!allowClick) return;
-    changeWagonPlace('castle');
+  var cursor_type;
+  if(side === 'bottom'){
+    cursor_type='not-allowed'
+  }else if(side === 'top'&& allowClick){
+    cursor_type = 'pointer';
   }
 
+
+  const onClick = () => {
+    if (cursor_type === 'not-allowed') {
+      return;
+    }else if(allowClick){
+      changeWagonPlace('castle');
+    }
+  }
+  
   return (
     <div
       className={"qg_land " + 'qg_land_side_' + side}
+      
       onClick={allowClick?onClick:undefined}
       style={{
-        cursor: allowClick? 'pointer' : 'not-allowed'
+        cursor: cursor_type
       }}
     >
       {side === 'top' && (
