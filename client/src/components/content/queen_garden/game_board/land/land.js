dcb66ae71329ffa5ad894d74188682386ecba98d
ggolan14 <guy.golan@ymail.com>
2024-01-30 12:05:51 +0200

    remove printouts

--- client/src/components/content/queen_garden/game_board/land/land.js
+++ client/src/components/content/queen_garden/game_board/land/land.js
@@ -5,8 +5,7 @@ import {CastleName} from "../castle/castle_name";
 import {QueenGardenGameBoardContext} from "../../context/qg_board_context";
 
 export const Land = ({side}) => {
-  console.log("---> side="+side);
-  
+   
   const {gameState, changeWagonPlace} = useContext(QueenGardenGameBoardContext);
 
   const allowClick = !gameState.disable_castle_click;
