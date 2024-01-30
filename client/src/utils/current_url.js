a033628ad6744ccdb695ceab811ed7b1aaf4c9a9
ggolan14 <guy.golan@ymail.com>
2024-01-30 15:47:53 +0200

    change client url from local to cloud

--- client/src/utils/current_url.js
+++ client/src/utils/current_url.js
@@ -1,5 +1,5 @@
 export const CURRENT_URL = () => {
-    // return 'https://k-lab.iem.technion.ac.il';
-    return 'http://localhost:4000';
+    return 'https://k-lab.iem.technion.ac.il';
+    //return 'http://localhost:4000';
     // return '';
 }
