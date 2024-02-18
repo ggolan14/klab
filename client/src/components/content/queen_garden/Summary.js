915c751b85905bd0e2b922d227964eef0df8799c
ggolan14 <guy.golan@ymail.com>
2024-02-18 13:34:21 +0200

    minor change

--- client/src/components/content/queen_garden/Summary.js
+++ client/src/components/content/queen_garden/Summary.js
@@ -6,7 +6,7 @@ const Summary = (props) => {
      return (
         <label>
             Based on the points you earned, your bonus payment is {props.SignOfReward}{final_bonus}.<br/>
-            You will get your bonus after all participants will finish the study<br/>
+            You will get your bonus after all participants will finish the games<br/>
             Please press the button below to complete the exit survey.<br/>
             The short survey includes only a few questions, after which you will get your completion code.<br/>
             Important: we will not be able to pay you if you do not complete the survey!
