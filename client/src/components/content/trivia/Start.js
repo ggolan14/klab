21b03b27ba1eb4d84a5e9768053eb2b0ef2678cf
ggolan14 <guy.golan@ymail.com>
2024-04-17 14:43:45 +0300

    improve Trivia game

--- client/src/components/content/trivia/Start.js
+++ client/src/components/content/trivia/Start.js
@@ -5,6 +5,7 @@ import practice_questions from './practice_questions.json';
 import {getTimeDate} from "../../../utils/app_utils";
 import {NewLogs} from "../../../actions/logger";
 import {DebuggerModalView, KeyTableID} from "../../screens/gameHandle/game_handle";
+import { number } from 'prop-types';
 const ThisExperiment = 'Trivia';
 
 
@@ -17,6 +18,7 @@ let SUM_OF_CUPS = 0;
 let GameSet = {};
 let PaymentsSettings = null;
 let GameCondition = null;
+let NUM_OF_PRACTICE_QUESTIONS=4;
 
 
 /*
@@ -67,113 +69,123 @@ class Start extends Component {
       showQuestion: true,
       yesClickCount: 0,
       practiceMode: true,
+      isLast: false,
       gameCondition:GameCondition,
       
     };
   }
 
   handleNext = () => {
-    const { currentQuestionIndex, practiceMode } = this.state;
+    console.log("---> I have an answer was clicked")
+    const { currentQuestionIndex, practiceMode,gameCondition ,isLast} = this.state;
     let questions = [];
-    console.log("---> in handleNext  practiceMode="+practiceMode)
-    if (practiceMode) {
-      questions = practice_questions;
-    } else {
-      questions = trivia_questions;
+    questions = trivia_questions;
+    console.log("---> questions.length="+questions.length + "  gameCondition="+gameCondition +"  currentQuestionIndex="+ currentQuestionIndex)
+    var lastIndex=0;
+    if(gameCondition === "OneShot"){
+      console.log("----------> OneShot lastIndex="+lastIndex)
+    lastIndex = 4;
     }
-    console.log("---> questions.length="+questions.length)
-    const lastIndex = questions.length - 1;
+    else{
+    lastIndex = questions.length-1;
+    console.log("----------> Repeated lastIndex="+lastIndex)
+    }
     console.log("---> in handleNext  practiceMode="+practiceMode+"  lastIndex="+lastIndex+ "  currentQuestionIndex="+currentQuestionIndex)
 
     // Check if it's the last question in practice mode
-    if (practiceMode && currentQuestionIndex === lastIndex) {
-      console.log("---> in handleNext 11111");
-      this.setState({
-        practiceMode: false,
+    console.log("---> Before in handleNext  currentQuestionIndex="+currentQuestionIndex)
+    if (currentQuestionIndex == lastIndex+1) {
+      console.log("---> in handleNext setting isLast to true");
+      this.setState(prevState => ({
+        isLast: true,
+        showConfirmation: true,
         currentQuestionIndex: 0 // Reset to the first question of trivia mode
-      });
+     }));
     } else {
       console.log("---> in handleNext 22222");
+      const correctAnswer = questions[currentQuestionIndex].answers.find(answer => answer.option === questions[currentQuestionIndex].correct_answer).text;
       // Increment the index to show the next question
       this.setState(prevState => ({
-        currentQuestionIndex: Math.min(prevState.currentQuestionIndex + 1, lastIndex)
+        currentQuestionIndex: Math.min(prevState.currentQuestionIndex + 1, lastIndex),
+        showConfirmation: true,
+        correctAnswer: correctAnswer,
+        showQuestion: false
       }));
-
+      console.log("---> After in handleNext  currentQuestionIndex="+currentQuestionIndex)
 
       
 
 
      // if (!practiceMode) {
-        const correctAnswer = questions[currentQuestionIndex].answers.find(answer => answer.option === questions[currentQuestionIndex].correct_answer).text;
-
-        this.setState({
-          showConfirmation: true,
-          correctAnswer: correctAnswer,
-          showQuestion: false
-        });
+       
      // }
       
     }
   };
 
   handleConfirmation = (confirmed) => {
-    console.log("-------> NUM of clicks=" + this.state.yesClickCount);
-    const { yesClickCount, practiceMode } = this.state;
-    if (!practiceMode) {
-        console.log("Before setState()  yesClickCount=" + yesClickCount + "   confirmed=" + confirmed);
+    
+    const { yesClickCount, practiceMode,isLast } = this.state;
+    console.log("#####> yes button was clicked NUM of clicks=" + yesClickCount+"    isLast="+isLast);
+    //const updatedYesClickCount = confirmed ? yesClickCount + 1 : yesClickCount;
+  if(isLast){
+  console.log("Before setState()  yesClickCount=" + yesClickCount + "   confirmed=" + confirmed);
 
-        // Increment yesClickCount
-        const updatedYesClickCount = confirmed ? yesClickCount + 1 : yesClickCount;
+  // Increment yesClickCount
+  const updatedYesClickCount = confirmed ? yesClickCount + 1 : yesClickCount;
 
-        // Update state and call insertGameLine in the setState callback
-        this.setState({
-            showResult: true,
-            showConfirmation: false,
-            yesClickCount: updatedYesClickCount,
-        }, () => {
-            const db_row = {
-                NumOfYesAnswers: updatedYesClickCount, // Use the updated count
-                GameCondition: GameCondition,
-            };
-            // Call insertGameLine with updated db_row
-            this.props.insertGameLine(db_row);
+  // Update state and call insertGameLine in the setState callback
+  this.setState(prevState => ({
+      showResult: true,
+      showConfirmation: false,
+      yesClickCount: confirmed ? ((prevState.yesClickCount)+1) : prevState.yesClickCount
+  }), () => {
+      const db_row = {
+          NumOfYesAnswers: updatedYesClickCount, // Use the updated count
+          GameCondition: GameCondition,
+      };
+      // Call insertGameLine with updated db_row
+      this.props.insertGameLine(db_row);
 
-            // Call sendGameDataToDB after insertGameLine in the callback
-            const current_time = getTimeDate();
-            const summary_args = {
-                //game_points: 56,
-            };
-            this.props.sendGameDataToDB().then(
-                res => {
-                    NewLogs({
-                        user_id: UserId,
-                        exp: ThisExperiment,
-                        running_name: RunningName,
-                        action: 'G.E.S',
-                        type: 'LogGameType',
-                        more_params: {
-                            game_points: "TBD",
-                            local_t: current_time.time,
-                            local_d: current_time.date,
-                        },
-                    }).then((res) => {});
-                    this.props.callbackFunction('FinishGame', { need_summary: true, args: summary_args });
-                }
-            );
-        });
-    } else {
-        console.log("Before setState() 222")
-        this.setState({
-            showConfirmation: false,
-            showQuestion: true
-        });
+      // Call sendGameDataToDB after insertGameLine in the callback
+      const current_time = getTimeDate();
+      const summary_args = {
+          //game_points: 56,
+      };
+      this.props.sendGameDataToDB().then(
+          res => {
+              NewLogs({
+                  user_id: UserId,
+                  exp: ThisExperiment,
+                  running_name: RunningName,
+                  action: 'G.E.S',
+                  type: 'LogGameType',
+                  more_params: {
+                      game_points: "TBD",
+                      local_t: current_time.time,
+                      local_d: current_time.date,
+                  },
+              }).then((res) => {});
+              this.props.callbackFunction('FinishGame', { need_summary: true, args: summary_args });
+          }
+      );
+  });
+}
+    if(practiceMode){
+      this.setState(prevState => ({
+        showConfirmation: false,
+        showQuestion: true
+      }));
+      
     }
+    
 };
 
 
 render() {
   const { currentQuestionIndex, showConfirmation, correctAnswer, showResult, showQuestion, yesClickCount, practiceMode, gameCondition} = this.state;
-  let questions = practiceMode ? practice_questions : trivia_questions;
+  console.log("---> in render   currentQuestionIndex="+currentQuestionIndex +"   showConfirmation="+showConfirmation +"   correctAnswer="+correctAnswer +"   showResult="+showResult +"   showQuestion="+showQuestion +"   yesClickCount="+yesClickCount+"    practiceMode="+practiceMode)
+  let questions = trivia_questions;
   const question = questions[currentQuestionIndex];
   const { answers } = question;
 
@@ -181,7 +193,7 @@ render() {
     <div className="trivia-container">
       {showQuestion && (
         <div>
-          {practiceMode && <span style={{ fontWeight: 'bold', color: 'red' }}>This is practice round</span>}
+          {currentQuestionIndex < NUM_OF_PRACTICE_QUESTIONS && <span style={{ fontWeight: 'bold', color: 'red' }}>This is practice round</span>}
           <p>Please read the following question. Pick your best answer and keep it in mind.</p>
           <p>{question.question}</p>
           <ul>
@@ -207,7 +219,7 @@ render() {
           {!showConfirmation && <button onClick={this.handleNext}>I have an answer in my mind</button>}
           {showConfirmation && (
             <div>
-              {practiceMode && <span style={{ fontWeight: 'bold', color: 'red' }}>This is practice round</span>}
+              {currentQuestionIndex <= NUM_OF_PRACTICE_QUESTIONS  && <span style={{ fontWeight: 'bold', color: 'red' }}>This is practice round</span>}
               <p>Correct answer is: {correctAnswer}</p>
               <p>Is this the answer you had in mind?</p>
               <button onClick={() => this.handleConfirmation(true)}>Yes</button>
