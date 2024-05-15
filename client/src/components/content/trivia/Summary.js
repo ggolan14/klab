ea2c1cb0edcccca877dfac6b845dc8d39c8c6cc1
ggolan14 <guy.golan@ymail.com>
2024-05-15 10:37:26 +0300

    Support finish screen

--- client/src/components/content/trivia/Summary.js
+++ client/src/components/content/trivia/Summary.js
@@ -7,11 +7,10 @@ const Summary = ({SummaryArgs}) => {
    
      return (
         <label>
-            Based on the points you earned, your bonus payment is {SummaryArgs.sign_of_reward}{SummaryArgs.reward_sum}.<br/>
-            You will get your bonus after all participants will finish the games<br/>
-            Please press the button below to complete the exit survey.<br/>
-            The short survey includes only a few questions, after which you will get your completion code.<br/>
-            Important: we will not be able to pay you if you do not complete the survey!
+            Thank you for your participation.
+            You earned a bounus payment of {SummaryArgs.sign_of_reward}{SummaryArgs.reward_sum} which will be added to your participation payment.<br/>
+            In order to recieve your compensation, please press on the button below.
+
         </label>
     )
 };
