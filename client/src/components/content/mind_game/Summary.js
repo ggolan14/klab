99c7ce1734711764a05e853d7084a37de04894f9
ggolan14 <guy.golan@ymail.com>
2024-10-13 09:10:53 +0300

    Change summary to be same as Trivia game

--- client/src/components/content/mind_game/Summary.js
+++ client/src/components/content/mind_game/Summary.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import {DebuggerModalView} from "../../screens/gameHandle/game_handle";
+import React, { useEffect } from 'react';
+import { DebuggerModalView } from "../../screens/gameHandle/game_handle";
 import { formatPrice } from '../../utils/StringUtils';
 
+const Summary = ({ SummaryArgs }) => {
+  
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = "Are you sure you want to leave this page? Your progress may not be saved."; // This message may not be shown in some browsers, but the default popup will appear.
+    };
 
+    window.addEventListener('beforeunload', handleBeforeUnload);
 
-const Summary = ({SummaryArgs}) => {
-   
-     return (
-        <label>
-            Thank you for your participation.
-            Your bonus is {formatPrice(SummaryArgs.reward_sum , SummaryArgs.sign_of_reward)} , and will be added to your participation payment.<br/>
-            To receive your payment, please click the button below.
-        </label>
-    )
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  return (
+    <label>
+      Thank you for your participation.
+      Your bonus is {formatPrice(SummaryArgs.reward_sum, SummaryArgs.sign_of_reward)}, and will be added to your participation payment.<br />
+      To receive your payment, please <b>copy the completion code</b> below and <b>click on the "Finish Game" button.</b><br/>It is essential to click <b>"Finish Game"</b> button for your data to be recorded!
+    </label>
+  );
 };
 
 export default Summary;
