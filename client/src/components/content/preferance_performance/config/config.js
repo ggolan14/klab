936c6f5e3c319320b6248cbcc2d4c7ba4cba3063
ggolan14 <guy.golan@ymail.com>
2024-10-30 11:27:27 +0200

    support blocks and steps configuration in config file

---
+++ client/src/components/content/preferance_performance/config/config.js
@@ -0,0 +1,7 @@
+export const config = {
+    X_1: 2, // Number of ShortBlocks
+    X_2: 3, // Number of LongBlocks
+    Y_1: 2, // Number of ShortTrials per ShortBlock
+    Y_2: 3  // Number of LongTrials per LongBlock
+  };
+  
