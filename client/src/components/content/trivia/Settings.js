a411f1c00f6da2887fac7947cbaa052f3f699dc0
ggolan14 <guy.golan@ymail.com>
2024-12-17 12:44:46 +0200

    Refactoring - Extract General and Payment sections to common components

--- client/src/components/content/trivia/Settings.js
+++ client/src/components/content/trivia/Settings.js
@@ -1,234 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {DivContainer} from "../../screens/settings/elements_builder";
-import {convertPointsRatio} from "../../screens/settings/settings";
-import ColorPicker from "../../layout/colorPicker/color_picker";
-//import './togStyles.css';
+import { DivContainer } from "../../screens/settings/elements_builder";
+import { convertPointsRatio } from "../../screens/settings/settings";
+import { generalSettings,paymentSettings } from '../../utils/settingsConfig';
 
-
-
-
-
-const game_tooltip = {
-    game1: (
-        <label>Basic game</label>
-    ),
-    game2: (
-        <>
-            <label>Punishing "give-up" selections:</label>
-            <label>Every time a "give-up" key is selected,</label>
-            <label>F points are subtracted from the final trial's payoff.</label>
-        </>
-    ),
-    game3: (
-        <>
-            <label>Punishing exploitation of L results:</label>
-            <label>Every time an "L" key is selected,</label>
-            <label>F points are subtracted from the final trial's payoff.</label>
-        </>
-    ),
-    game4: (
-        <>
-            <label>Punishing "give-up" selections & exploitation of L results:</label>
-            <label>This game is combination of g=2 and g=3.</label>
-        </>
-    ),
-    game5: (
-        <>
-            <label>Rewarding exploration of new “try” keys:</label>
-            <label>Every time a new "try" key is selected,</label>
-            <label>F points are added to the final trial's payoff.</label>
-        </>
-    ),
-    game6: (
-        <>
-            <label>Rewarding every “try” selection:</label>
-            <label>Every time a "try" key is selected,</label>
-            <label>F points are added to the final trial's payoff.</label>
-        </>
-    ),
-
-};
-
-const Settings = ({game_settings, changeSettings, LAST_SETTING_NAME, versions_list, exp_more_settings}) => {
-
-    const general = [
-        {
-            type: 'Input',
-            label: 'Version:',
-            show: true,
-            class_name: 'admin-settings-input-big ' + (game_settings.version === 'test' ? ' disabledElem ' : '') + ((game_settings.version !== LAST_SETTING_NAME && versions_list.indexOf(game_settings.version) > -1) ? 'highlight_error_input' : ''),
-            value: game_settings.version,
-            input_type: 'text',
-            callback: game_settings.version === 'test' ? () => {} : value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'version',
-                key2: null,
-                value,
-            })
-        },
-        {
-            type: 'Select',
-            label: 'Practice:',
-            show: true,
-            options: [
-                {label: 'Yes', value: 'Yes'},
-                {label: 'No', value: 'No'},
-            ],
-            value: game_settings.general.w_p,
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'general',
-                key2: 'w_p',
-                value,
-            })
-        },
-        {
-            type: 'Select',
-            label: 'Consent form:',
-            show: true,
-            options: ['Yes', 'No'],
-            value: game_settings.general.consent_form,
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'general',
-                key2: 'consent_form',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Redirect to:  ',
-            show: true,
-            class_name: 'admin-settings-input-big ',
-            value: game_settings.general.redirect_to,
-            input_type: 'text',
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'general',
-                key2: 'redirect_to',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Action time:',
-            show: true,
-            class_name: '',
-            value: game_settings.general.action_time,
-            input_type: 'number',
-            step: 0.1,
-            min: 0,
-            label_after: '[mm:ss]',
-            pattern: 'MM:SS',
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'general',
-                key2: 'action_time',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Second warning:',
-            show: true,
-            class_name: '',
-            value: game_settings.general.second_warning,
-            input_type: 'number',
-            step: 0.1,
-            min: 0,
-            label_after: 'second',
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'general',
-                key2: 'second_warning',
-                value,
-            })
-        },
-    ];
-
-    const payments = [
-        {
-            type: 'Select',
-            label: 'Sign of reward:',
-            show: true,
-            options: ['₪', '£', '$', '€'],
-            value: game_settings.payments.sign_of_reward,
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'payments',
-                key2: 'sign_of_reward',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Show up fee:',
-            show: true,
-            class_name: '',
-            value: game_settings.payments.show_up_fee,
-            input_type: 'number',
-            step: 0.1,
-            min: 0,
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'payments',
-                key2: 'show_up_fee',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Exchange ratio:',
-            show: true,
-            class_name: '',
-            value: game_settings.payments.exchange_ratio,
-            input_type: 'number',
-            step: 0.1,
-            min: 0,
-            // label_after: `${game_settings.payments.sign_of_reward}1=${game_settings.payments.exchange_ratio} ${game_settings.payments.exchange_ratio !== 1 ? 'points' : 'point'}         1 point=${game_settings.payments.sign_of_reward}${game_settings.payments.exchange_ratio === 0 ? 0 : 1/game_settings.payments.exchange_ratio}`,
-            label_after: convertPointsRatio(game_settings.payments.sign_of_reward, game_settings.payments.exchange_ratio),
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'payments',
-                key2: 'exchange_ratio',
-                value,
-            })
-        },
-        {
-            type: 'Input',
-            label: 'Bonus endowment:',
-            show: true,
-            class_name: '',
-            value: game_settings.payments.bonus_endowment,
-            input_type: 'number',
-            step: 0.1,
-            min: 0,
-            callback: value => changeSettings({
-                settings_of: 'game_settings',
-                key: 'payments',
-                key2: 'bonus_endowment',
-                value,
-            })
-        },
-    ];
-
-    const modified_info = [
-        {
-            type: 'Label',
-            label: 'Last modified:',
-            show: true,
-            class_name_b: 'settings-modified-lbl',
-            second_label: game_settings.last_modified
-        },
-        {
-            type: 'Label',
-            label: 'Date modified:',
-            show: true,
-            class_name_b: 'settings-modified-lbl',
-            second_label: game_settings.date_modified
-        },
-    ];
+const Settings = ({ game_settings, changeSettings, LAST_SETTING_NAME, versions_list }) => {
+    const general = generalSettings(game_settings, LAST_SETTING_NAME, versions_list, changeSettings);
+    const payments = paymentSettings(game_settings, changeSettings, convertPointsRatio);
 
     const game_elements = [
         {
@@ -262,50 +40,31 @@ const Settings = ({game_settings, changeSettings, LAST_SETTING_NAME, versions_li
             })
         }
     ]
+
     return (
         <>
-            <DivContainer
-                className='admin-settings-section admin-settings-section-raw'
-            >
+            <DivContainer className='admin-settings-section admin-settings-section-raw'>
                 <DivContainer
                     className='admin-settings-section-col admin-settings-section-h'
-                    elements={[{
-                        type: 'Label',
-                        label: 'General:',
-                        show: true
-                    }]}
+                    elements={[{ type: 'Label', label: 'General:', show: true }]}
                 />
                 <DivContainer
                     className='admin-settings-section-raw admin-settings-section-b'
                     elements={general}
                 />
-
-                <DivContainer
-                    className='admin-settings-section-raw admin-settings-section-modified_info admin-settings-section-b'
-                    elements={modified_info}
-                />
             </DivContainer>
 
-            <DivContainer
-                className='admin-settings-section admin-settings-section-raw'
-            >
+            <DivContainer className='admin-settings-section admin-settings-section-raw'>
                 <DivContainer
                     className='admin-settings-section-col admin-settings-section-h'
-                    elements={[{
-                        type: 'Label',
-                        label: 'Payment:',
-                        show: true
-                    }]}
+                    elements={[{ type: 'Label', label: 'Payment:', show: true }]}
                 />
                 <DivContainer
                     className='admin-settings-section-raw admin-settings-section-b'
                     elements={payments}
                 />
-                
             </DivContainer>
-            <DivContainer
-                className='admin-settings-section admin-settings-section-raw'
-            >
+            <DivContainer className='admin-settings-section admin-settings-section-raw' >
             <DivContainer
                     className='admin-settings-section-col admin-settings-section-h'
                     elements={[{
@@ -314,28 +73,20 @@ const Settings = ({game_settings, changeSettings, LAST_SETTING_NAME, versions_li
                         show: true
                     }]}
                 />
-                <DivContainer
+            <DivContainer
                     className='admin-settings-section-raw admin-settings-section-b'
                     elements={game_elements}
                 />
-                </DivContainer>
-
-
-
-
-
+             </DivContainer>
         </>
-    )
+    );
 };
 
 Settings.propTypes = {
-    exp: PropTypes.string.isRequired,
     game_settings: PropTypes.object.isRequired,
     changeSettings: PropTypes.func.isRequired,
     LAST_SETTING_NAME: PropTypes.string,
     versions_list: PropTypes.array.isRequired,
-    exp_: PropTypes.string.isRequired,
 };
 
 export default Settings;
-
