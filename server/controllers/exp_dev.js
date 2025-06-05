50eac2d1cad35c0b8ed6f31f2fa163bb56002ae9
ggolan14 <guy.golan@ymail.com>
2025-06-05 12:25:54 +0300

    fix typo

--- server/controllers/exp_dev.js
+++ server/controllers/exp_dev.js
@@ -115,7 +115,7 @@ const getExpDev = asyncHandler(async (req, res) => {
         }
     }
     catch (e) {
-        res.json({error: 'Some Error happened4'});
+        res.json({error: 'Some Error happened'});
     }
 })
 
