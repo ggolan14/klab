8e7288c7656686d454efaac32449ced146f28a3b
ggolan14 <guy.golan@ymail.com>
2025-06-25 09:47:37 +0300

    print to log server url

--- client/src/utils/current_url.js
+++ client/src/utils/current_url.js
@@ -1,7 +1,6 @@
 export const CURRENT_URL = () => {
-   // return 'https://k-lab.iem.technion.ac.il';
-   // return 'http://localhost:4000';
+   console.log("---> connecting to server which is running on "+(process.env.REACT_APP_SERVER_URL || 'http://localhost:4000'))
    return process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
-    // return '';
+  
 }
 
