ba000b442b531d457a058af2e969447ea74609e3
ggolan14 <guy.golan@ymail.com>
2025-07-27 12:03:51 +0300

    change text of final message

--- client/src/components/content/dots_mind_game_benchmark/Summary.js
+++ client/src/components/content/dots_mind_game_benchmark/Summary.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const Summary = ( debug_args ) => {
-     let final_score = debug_args.GameBonus;
-     
-     
-     
-    
-     console.log("---> final_score="+final_score+"   debug_args"+debug_args.Game)
-     return (
-        <label>
-            Your bonus is : <b style={{ fontSize: "24px" }}>{final_score} {debug_args.SignOfReward}</b><br/>
-           
+const Summary = (debug_args) => {
+    let final_score = debug_args.GameBonus;
+
+
+
+
+    console.log("---> final_score=" + final_score + "   debug_args" + debug_args.Game)
+    return (
+
+        <label style={{ fontSize: "24px", width: "900px" }}>
+            You have earned <b style={{ fontSize: "24px" }}>{final_score} {debug_args.SignOfReward}</b>.<br />
+            Before you go, we would like to disclose that all the information provided to you in this study was true, except for one detail that may have been inaccurate.
+            Different information was provided to different participants in this study.
+            Specifically, you may have received inaccurate information on the percentage of trials in which more dots appeared on each side.
+            This was done with the goal of understanding how different types of information affect people's decisions
+            in the dots task.
+            <br /><br />
+            <span style={{ fontWeight: "bold" }}>We thank you again for your participation.</span>
         </label>
+
+
     )
 };
 
